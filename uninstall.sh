e6a8500b70f58a375ba56eca992bd42705886cf9
Guille <guille0596@gmail.com>
2026-04-22 03:24:34 +0200

    Fix uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
-TMP_SCRIPT="/tmp/quick-dns-switcher-uninstall-$$.sh"
-cp "${SCRIPT_DIR}/qds/resources/bin/quick-dns-switcher-uninstall.sh" "${TMP_SCRIPT}"
-chmod 755 "${TMP_SCRIPT}"
+TMP_SCRIPT="/tmp/qds-uninstall-$$.sh"
+install -Dm755 "${SCRIPT_DIR}/qds/resources/bin/qds-uninstall.sh" "${TMP_SCRIPT}"
+
 exec bash "${TMP_SCRIPT}" "${@}"
